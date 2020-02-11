6b52c24ce510394867d78861e03ccae2655eb2b9
cc <274065539@qq.com>
2020-02-11 11:36:59 +0800

    2020.2.11

--- system/example/test.py
+++ system/example/test.py
@@ -8,20 +8,25 @@ from otsu import otsu_threshold
 import cv2
 from read_images import read_images
 from ACWE.ACWE import *
+import random
 
 startNum=0
 endNum=1200
 # img_height=255
 # img_width=255
 
-
 path = r'D:\carck_detect_system\crack'
 img,img_width,img_height,startNum,endNum=read_images(path,startNum,endNum)
 x,y,z = img.shape
-r=20
+r=10
 phi0 = initialize(x,y,z, x_center=r, y_center=r, z_center=r, radius=r-2)
-# a=acwe(img, phi0, max_iter=5, time_step=0.1, mu=0.1, v=0.1, lambda1=1, lambda2=1, epsilon=1)
-a=phi0
+a=CV(img, phi0, max_iter=30, time_step=100, mu=0.1, v=0.1, lambda1=1, lambda2=1, epison=10)
+# a=phi0
+# a=a.astype(np.uint8)
+# a=np.zeros((x,y,z),dtype=np.uint8)
+# a[30:50,30:50,30:50]=250+random.randint(0,8)
+print(img.shape,img.dtype,a.shape,a.dtype)
+# a=img
 print("img zise: ",img_height," ,",img_width,startNum,endNum)
 # a=grow(a,(0,0,0),1)
 
@@ -31,7 +36,8 @@ dataImporter = vtk.vtkImageImport()
 data_string = a.tostring()
 dataImporter.CopyImportVoidPointer(data_string, len(data_string))
 # The type of the newly imported data is set to unsigned char (uint8)
-dataImporter.SetDataScalarTypeToUnsignedChar()
+# dataImporter.SetDataScalarTypeToUnsignedChar()
+dataImporter.SetDataScalarTypeToFloat()
 # Because the data that is imported only contains an intensity value
 #  (it isnt RGB-coded or someting similar), the importer must be told this is the case.
 dataImporter.SetNumberOfScalarComponents(1)
@@ -47,13 +53,10 @@ v16.SetDataSpacing(1,1,1)
 # v16.SetAllow8BitBMP(16)
 v16.Update()
 
-###############################################################################################
-colors = vtk.vtkNamedColors()
-# This is a simple volume rendering example that
-# uses a vtkFixedPointVolumeRayCastMapper
 
-# Create the standard renderer, render window
-# and interactor.
+colors = vtk.vtkNamedColors()
+
+# Create the RenderWindow, Renderer and Interactor.
 ren1 = vtk.vtkRenderer()
 
 renWin = vtk.vtkRenderWindow()
@@ -62,71 +65,44 @@ renWin.AddRenderer(ren1)
 iren = vtk.vtkRenderWindowInteractor()
 iren.SetRenderWindow(renWin)
 
+iso = vtk.vtkContourFilter()
+iso.SetInputConnection(v16.GetOutputPort())
+iso.SetValue(0, 0)
 
-# Create transfer mapping scalar value to opacity.
-opacityTransferFunction = vtk.vtkPiecewiseFunction()
-opacityTransferFunction.AddPoint(-0.5, 0.01)
-opacityTransferFunction.AddPoint(0.5, 0.5)
-# opacityTransferFunction.ClampingOff()
+isoMapper = vtk.vtkPolyDataMapper()
+isoMapper.SetInputConnection(iso.GetOutputPort())
+isoMapper.ScalarVisibilityOff()
 
-# Create transfer mapping scalar value to color.
-colorTransferFunction = vtk.vtkColorTransferFunction()
-colorTransferFunction.AddRGBPoint(0.0, 0.0, 0.0, 0.7)
-# colorTransferFunction.AddRGBPoint(50, 0.0, 0.0, 0.0)
-# colorTransferFunction.AddRGBPoint(100, 0.0, 0.0, 0.0)
-# # colorTransferFunction.AddRGBPoint(150, 0.0, 0.7, 0.7)
-# colorTransferFunction.AddRGBPoint(255, 0.0, 0.0, 0.0)
+isoActor = vtk.vtkActor()
+isoActor.SetMapper(isoMapper)
+isoActor.GetProperty().SetColor(colors.GetColor3d("Banana"))
+isoActor.GetProperty().SetDiffuseColor(1, .94, .25)
+# 设置高光照明系数
+isoActor.GetProperty().SetSpecular(.1)
+# 设置高光能量
+isoActor.GetProperty().SetSpecularPower(100)
+isoActor.GetProperty().SetOpacity(0.5)
 
-#Creat opacity gradient function
-gradientTransferFunction=vtkPiecewiseFunction()
-gradientTransferFunction.AddPoint(0,0.0)
-gradientTransferFunction.AddPoint(255,2.0)
-# The property describes how the data will look.
-volumeProperty = vtk.vtkVolumeProperty()
-volumeProperty.SetColor(colorTransferFunction)
-volumeProperty.SetScalarOpacity(opacityTransferFunction)
-volumeProperty.SetGradientOpacity(gradientTransferFunction)
+outline = vtk.vtkOutlineFilter()
+outline.SetInputConnection(v16.GetOutputPort())
 
-volumeProperty.ShadeOn()
-volumeProperty.SetAmbient(0.5)  #设置环境光系数
-volumeProperty.SetDiffuse(1)  #设置散射光系数
-volumeProperty.SetSpecular(0.5)  #设置反射光系数
-volumeProperty.SetSpecularPower(50)
-#设置灯光
-myLight2 = vtkLight()
-myLight2.PositionalOn()
-myLight2.SetColor(1, 1, 1)
-myLight2.SetPosition(-9999999, -9999999, -9999999)
-myLight2.SetFocalPoint(ren1.GetActiveCamera().GetFocalPoint())
-# ren1.AddLight(myLight2)
+outlineMapper = vtk.vtkPolyDataMapper()
+outlineMapper.SetInputConnection(outline.GetOutputPort())
 
-volumeProperty.SetInterpolationTypeToLinear()
-# volumeProperty.SetInterpolationTypeToNearest()
+outlineActor = vtk.vtkActor()
+outlineActor.SetMapper(outlineMapper)
 
-# The mapper / ray cast function know how to render the data.
-# volumeMapper = vtk.vtkFixedPointVolumeRayCastMapper()
-# volumeMapper=vtkGPUVolumeRayCastMapper()
-volumeMapper=vtkSmartVolumeMapper()
-volumeMapper.SetRequestedRenderModeToRayCast()
-volumeMapper.SetRequestedRenderModeToGPU()
-# volumeMapper.SetInterpolationModeToLinear()
-volumeMapper.SetInputConnection(v16.GetOutputPort())
+# Add the actors to the renderer, set the background and size.
+#
+ren1.AddActor(outlineActor)
+ren1.AddActor(isoActor)
+ren1.SetBackground(colors.GetColor3d("SlateGray"))
+renWin.SetSize(640, 512)
 
-# The volume holds the mapper and the property and
-# can be used to position/orient the volume.
-volume = vtk.vtkVolume()
-volume.SetMapper(volumeMapper)
-volume.SetProperty(volumeProperty)
-
-ren1.AddVolume(volume)
-ren1.SetBackground(colors.GetColor3d("White"))
-ren1.SetGradientBackground(True)
-ren1.GetActiveCamera().Azimuth(45)
-ren1.GetActiveCamera().Elevation(30)
-ren1.ResetCameraClippingRange()
+# Render the image.
+#
 ren1.ResetCamera()
-
-renWin.SetSize(600, 600)
+ren1.GetActiveCamera().Azimuth(30)
+ren1.GetActiveCamera().Elevation(30)
 renWin.Render()
-
 iren.Start()
